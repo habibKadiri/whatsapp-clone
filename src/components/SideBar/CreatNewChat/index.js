bcfc499185ea7b4aab1715bfc454ebd33e051d4e
Habib Kadiri <kadirit@hotmail.com>
2020-08-21 21:09:53 +0200

    Fixed UI for creating room

--- src/components/SideBar/CreatNewChat/index.js
+++ src/components/SideBar/CreatNewChat/index.js
@@ -26,7 +26,8 @@ const CreateNewChat = () => {
     console.log(open);
 
 
-    const createChat = () => {
+    const createChat = (e) => {
+        e.preventDefault()
         if (newName) {
             db.collection('rooms').add({
                 name: newName
@@ -56,29 +57,32 @@ const CreateNewChat = () => {
             </Container>
             <div>
                 <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-                    <DialogTitle id="form-dialog-title">Create</DialogTitle>
-                    <DialogContent>
-                        <DialogContentText>
-                            Please enter a room name:
-                        </DialogContentText>
-                        <TextField
-                            autoFocus
-                            margin="dense"
-                            id="name"
-                            label="Room name"
-                            type="text"
-                            onChange={handleNewName}
-                            fullWidth
-                        />
-                    </DialogContent>
-                    <DialogActions>
-                        <Button onClick={handleClose} color="primary">
-                            Cancel
-                        </Button>
-                        <Button onClick={createChat} color="primary">
-                            Create
-                        </Button>
-                    </DialogActions>
+                    <form onSubmit={createChat}>
+                        <DialogTitle id="form-dialog-title">Create</DialogTitle>
+                        <DialogContent>
+                            <DialogContentText>
+                                Please enter a room name:
+                            </DialogContentText>
+                            <TextField
+                                autoFocus
+                                margin="dense"
+                                id="name"
+                                label="Room name"
+                                type="text"
+                                onChange={handleNewName}
+                                required
+                                fullWidth
+                            />
+                        </DialogContent>
+                        <DialogActions>
+                            <Button onClick={handleClose} color="primary">
+                                Cancel
+                            </Button>
+                            <Button type="submit" color="primary">
+                                Create
+                            </Button>
+                        </DialogActions>
+                    </form>
                 </Dialog>
             </div>
         </>
